b7d3b8ea486c1d06208f9eefa56a7b253d8a96e1
Laura Kapitza <laura.kapitza@gmail.com>
2021-09-05 01:50:19 +0200

    navbar done

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -1,10 +1,29 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import './Navigation.css';
 
 function Navigation() {
+  const [display, setDisplay] = useState('none')
+
+
   return (
-    <div className="Navigation">
-      <p>Navbar</p>
-    </div>
+    <nav className="Navigation">
+      <Link to='/'>Laura Lorbeer</Link>
+      
+      <div className='icon' onClick = {() => {
+        if(display === 'block') setDisplay('none')
+        if(display === 'none') setDisplay('block')
+      }}>
+        <i className="fa fa-bars"></i>
+      </div>
+
+      <div className='nav-links' style={{display: window.screen.width >= 600 ? 'flex' : display}}>
+          <Link to='/work'>Work</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/contact'>Contact</Link>
+      </div>
+
+    </nav>
   );
 }
 
