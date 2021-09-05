cdcd8770964bd68ee07d28b74593d7eaf13a9281
Laura Kapitza <laura.kapitza@gmail.com>
2021-09-05 22:36:26 +0200

    starting json file for projects & starting home component

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -3,7 +3,33 @@ import './Home.css';
 function Home() {
   return (
     <div className="Home">
-      <p>Nice hompeage</p>
+      <header>
+        <h2>Hi, I'm Laura!</h2>
+        <div>
+          <p>I am a Full Stack Web Developer based in Paris</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel tempor libero, 
+            eu finibus lectus. Sed sed consectetur nisl. Ut placerat sagittis magna vel ultricies. 
+            Donec felis felis, egestas ut ligula et, gravida sagittis augue. Aenean id vestibulum magna. 
+          </p>
+        </div>
+      </header>
+
+      <main>
+        <h3>Recent Work</h3>
+
+        {/* TODO Add loop to add html for each project */}
+
+        <article>
+          <img src="https://via.placeholder.com/150" alt="" />
+          <div>
+            <h4>Project #1</h4>
+            <p>Here comes a little description about the project</p>
+          </div>
+        </article>
+
+      </main>
+
     </div>
   );
 }
--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -7,23 +7,28 @@ function Navigation() {
 
 
   return (
-    <nav className="Navigation">
-      <Link to='/'>Laura Lorbeer</Link>
+    <div className="Navigation">
+      <h1>
+        <Link to='/'>Laura Lorbeer</Link>
+      </h1>
+
+      <nav>
+        <div className='icon' onClick = {() => {
+          if(display === 'block') setDisplay('none')
+          if(display === 'none') setDisplay('block')
+        }}>
+          <i className="fa fa-bars"></i>
+        </div>
+
+        <div className='nav-links' style={{display: window.screen.width >= 600 ? 'flex' : display}}>
+            <Link to='/work'>Work</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
+        </div>
+      </nav>
       
-      <div className='icon' onClick = {() => {
-        if(display === 'block') setDisplay('none')
-        if(display === 'none') setDisplay('block')
-      }}>
-        <i className="fa fa-bars"></i>
-      </div>
 
-      <div className='nav-links' style={{display: window.screen.width >= 600 ? 'flex' : display}}>
-          <Link to='/work'>Work</Link>
-          <Link to='/about'>About</Link>
-          <Link to='/contact'>Contact</Link>
-      </div>
-
-    </nav>
+    </div>
   );
 }
 
